ef0b4e2df24308b707f546c9e3be681c327b1e0a
Miguel Coronel <m.coronel.seg@gmail.com>
2015-10-05 14:05:50 -0500

    Add complience test

---
+++ test/compliance_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+require "fixtures/sample_mail"
+
+class ComplianceTest < ActiveSupport::TestCase
+  include ActiveModel::Lint::Tests
+
+  def setup
+    @model = SampleMail.new
+  end
+  
+end
